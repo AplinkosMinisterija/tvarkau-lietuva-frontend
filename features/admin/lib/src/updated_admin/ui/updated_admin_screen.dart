12bbed03669039bc06994e39ef3a5d0451026f94
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2023-11-10 11:48:19 +0200

    Add new temporary files for updated admin dashboard development

---
+++ features/admin/lib/src/updated_admin/ui/updated_admin_screen.dart
@@ -0,0 +1,17 @@
+import 'package:core/core.dart';
+import 'package:flutter/material.dart';
+
+
+class UpdatedAdminScreen extends StatelessWidget {
+  const UpdatedAdminScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return Container();
+    });
+  }
+}
