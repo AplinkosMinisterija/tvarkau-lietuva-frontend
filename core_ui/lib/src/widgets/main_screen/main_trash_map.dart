c18f6aa21b2b0cee90c182ecd18f29cea700a06f
Karolis Vyčius <k.vycius@gmail.com>
2023-11-21 16:28:41 +0200

    Initial implementation for OpenStreet map

--- core_ui/lib/src/widgets/main_screen/main_trash_map.dart
+++ core_ui/lib/src/widgets/main_screen/main_trash_map.dart
@@ -1,11 +1,11 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:core_ui/core_ui.dart';
 import 'package:domain/report/report_library.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:domain/domain.dart';
-import 'information_window/custom_info_window.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainTrashMap extends StatefulWidget {
   const MainTrashMap({
@@ -29,156 +29,160 @@ class MainTrashMap extends StatefulWidget {
 }
 
 class _MainTrashMapState extends State<MainTrashMap> {
-  final CustomInfoWindowController _customTrashInfoWindowController =
-      CustomInfoWindowController();
-  BitmapDescriptor trashMarkerIcon = BitmapDescriptor.defaultMarker;
-  bool isShowMarkers = false;
-  bool isShowDumps = true;
-  Set<Marker> trashMarkers = {};
-  Set<Marker> _trashMarkers = {};
+  final PopupController _popupController = PopupController();
 
-  Future<void> voidAddTrashMarkers() async {
-    int index = 0;
-    Set<Marker> tempMarkers = {};
-    for (var element in widget.trashReports) {
-      tempMarkers.add(
-        Marker(
-            markerId: MarkerId(
-              element.name.toString() + index.toString(),
+  Set<Marker> trashMarkers = {};
+  late List<Marker> _markers;
+
+  List<Marker> getMarkers() {
+    return widget.trashReports
+        .map(
+          (r) => Marker(
+            key: ObjectKey(r),
+            point: LatLng(r.reportLat, r.reportLong),
+            width: 25,
+            height: 30,
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: Image.asset(
+                getTrashIconPath(r.status),
+                key: Key('trash-icon-${r.status}'),
+              ),
             ),
-            position: LatLng(
-              element.reportLat,
-              element.reportLong,
-            ),
-            icon: await BitmapDescriptor.fromAssetImage(
-                const ImageConfiguration(size: Size(25, 30)),
-                getTrashIconPath(element.status)),
-            onTap: () {
-              _customTrashInfoWindowController.addInfoWindow!(
-                InfoTrashWindowBox(
-                    title: element.name ?? '',
-                    imageUrls: element.imageUrls ?? [],
-                    status: element.status,
-                    date: element.reportDate,
-                    reportId: element.refId ?? '',
-                    onTap: () {
-                      widget.onInformationTap(element.refId ?? '1');
-                    }),
-                LatLng(
-                  element.reportLat,
-                  element.reportLong,
-                ),
-              );
-            }),
-      );
-      index++;
-    }
-    setState(() {
-      _trashMarkers = tempMarkers;
-    });
+          ),
+        )
+        .toList();
   }
 
-  late MapType _currentMapType;
-  final CameraPosition _lithuaniaCameraPosition =
-      const CameraPosition(target: LatLng(55.1736, 23.8948), zoom: 7.0);
+  final _initialCenter = const LatLng(55.1736, 23.8948);
+  final _defaultZoom = 7.0;
 
   @override
   void initState() {
-    voidAddTrashMarkers();
-    _currentMapType = MapType.normal;
+    _markers = getMarkers();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() {
-        widget.isHovering(true);
-      }),
-      onExit: (_) => setState(() {
-        widget.isHovering(false);
-      }),
-      child: Stack(
-        children: [
-          Column(
-            children: <Widget>[
-              SizedBox(
-                height: widget.width * 0.354,
-                width: widget.width * 0.625,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(32)),
-                  child: Stack(
-                    children: [
-                      GoogleMap(
-                        mapType: _currentMapType,
-                        initialCameraPosition: _lithuaniaCameraPosition,
-                        markers: _trashMarkers,
-                        myLocationButtonEnabled: true,
-                        myLocationEnabled: true,
-                        onMapCreated: (GoogleMapController controller) async {
-                          _customTrashInfoWindowController.googleMapController =
-                              controller;
-                        },
-                        onCameraMove: (position) {
-                          _customTrashInfoWindowController.onCameraMove!();
-                        },
-                        onTap: (position) {
-                          _customTrashInfoWindowController.hideInfoWindow!();
-                        },
-                      ),
-                      CustomInfoWindow(
-                        (top, left, width, height) => {},
-                        leftMargin: 200,
-                        controller: _customTrashInfoWindowController,
-                        isDump: widget.isShowDumps,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 110, right: 10),
-                        child: Align(
-                            alignment: Alignment.bottomRight,
-                            child: GoogleMapTypeButton(
-                              height: 40,
-                              width: 40,
-                              onPressed: () {
-                                showDialog<String>(
-                                    context: context,
-                                    builder: (BuildContext context) =>
-                                        MapTypeChangeDialog(
-                                            width: widget.width / 2.4,
-                                            currentMapType: _currentMapType,
-                                            onHover: (isHover) {},
-                                            onChangeTap: (MapType mapType) {
-                                              setState(() {
-                                                _currentMapType = mapType;
-                                              });
-                                            }));
+    return Stack(
+      children: [
+        Column(
+          children: <Widget>[
+            SizedBox(
+              height: widget.width * 0.4,
+              width: widget.width * 0.625,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(32)),
+                child: Stack(
+                  children: [
+                    PopupScope(
+                      popupController: _popupController,
+                      child: FlutterMap(
+                        options: MapOptions(
+                          initialZoom: _defaultZoom,
+                          initialCenter: _initialCenter,
+                          minZoom: 6,
+                          onTap: (_, __) => _popupController
+                              .hideAllPopups(), // Hide popup when the map is tapped.
+                        ),
+                        children: [
+                          TileLayer(
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            tileProvider: CancellableNetworkTileProvider(),
+                          ),
+                          RichAttributionWidget(
+                            animationConfig: const ScaleRAWA(),
+                            showFlutterMapAttribution: false,
+                            alignment: AttributionAlignment.bottomLeft,
+                            attributions: [
+                              TextSourceAttribution(
+                                'OpenStreetMap contributors',
+                                onTap: () => launchUrl(
+                                  Uri.parse(
+                                    'https://openstreetmap.org/copyright',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          MarkerClusterLayerWidget(
+                            options: MarkerClusterLayerOptions(
+                              maxClusterRadius: 60,
+                              size: const Size(40, 40),
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.all(50),
+                              disableClusteringAtZoom: 12,
+                              markers: _markers,
+                              popupOptions: PopupOptions(
+                                popupSnap: PopupSnap.markerTop,
+                                popupController: _popupController,
+                                popupBuilder: (_, marker) {
+                                  final report = (marker.key as ObjectKey).value
+                                      as ReportModel;
+
+                                  return InfoTrashWindowBox(
+                                    title: report.name ?? '',
+                                    imageUrls: report.imageUrls ?? [],
+                                    status: report.status,
+                                    date: report.reportDate,
+                                    reportId: report.refId ?? '',
+                                    onTap: () {
+                                      widget.onInformationTap(
+                                        report.refId ?? '',
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                              builder: (context, markers) {
+                                return MouseRegion(
+                                  cursor: SystemMouseCursors.click,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: Colors.teal,
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        markers.length.toString(),
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
                               },
-                            )),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 20,
-              left: 20,
             ),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: ReportTypeSwitcher(
-                width: widget.width,
-                isShowDumps: widget.isShowDumps,
-                onReportTypeChange: (bool value) {
-                  widget.onReportTypeChange(value);
-                },
-              ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 20,
+            left: 20,
+          ),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: ReportTypeSwitcher(
+              width: widget.width,
+              isShowDumps: widget.isShowDumps,
+              onReportTypeChange: (bool value) {
+                widget.onReportTypeChange(value);
+              },
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
