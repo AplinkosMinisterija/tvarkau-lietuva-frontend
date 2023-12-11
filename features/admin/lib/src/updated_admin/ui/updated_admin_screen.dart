03bce412008fd7938d8664b23ce7efd42f6a5c82
Elvinas <elviz.nejuz@gmail.com>
2023-12-11 11:39:45 +0200

    added map view

--- features/admin/lib/src/updated_admin/ui/updated_admin_screen.dart
+++ features/admin/lib/src/updated_admin/ui/updated_admin_screen.dart
@@ -1,5 +1,7 @@
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../common/custom_colors.dart';
 import '../common/custom_styles.dart';
@@ -30,7 +32,130 @@ class _UpdatedAdminScreenState extends State<UpdatedAdminScreen> {
           id: "1",
           name: "Pirmas ataskaitos modelis",
           type: "Eismo įvykis",
+          reportLong: 23.279652,
+          reportLat: 54.687157,
+          reportDate: "2021-07-20",
+          isVisible: true,
+          isDeleted: false,
+          email: "pavyzdys@el.pastas.lt",
+          address: "Vilnius, Lietuva",
+          refId: "REF123",
+          status: "gautas",
+          phone: "+37061234567",
+          comment:
+              "Šiandien, apsilankius miške, pastebėjau išmestus šiferio lakštus. Jų gana nemažas kiekis, maždaug apie 100 lakštų. Jie išmėtyti po gana didelę teritoriją. Kartu su šiferio lakštais yra išmesti ir seni baldai bei buitinė technika.",
+          workingHours: "I-V 8:00-17:00",
+          moreInformation: "Papildoma informacija",
+          imageUrls: [
+            "https://nuoroda.lt/nuotrauka1.jpg",
+            "https://nuoroda.lt/nuotrauka2.jpg",
+          ],
+          officerImageUrls: [
+            "https://nuoroda.lt/policijos_nuotrauka1.jpg",
+          ],
+          historyData: [
+            HistoryData(
+              user: "Vartotojas1",
+              date: "2021-07-21",
+              edits: [
+                Edits(field: "status", change: "Naujas statusas"),
+                Edits(field: "komentaras", change: "Atnaujintas komentaras"),
+              ],
+            ),
+          ],
+          statusRecords: [
+            StatusRecords(status: "Naujas", date: "2021-07-20"),
+            StatusRecords(status: "Aptarnaujama", date: "2021-07-21"),
+          ],
+        ),
+        // ... Two more ReportModel instances
+      ];
+  List<ReportModel> get mockDumpModels => [
+        ReportModel(
+          id: "1",
+          name: "Pirmas ataskaitos modelis",
+          type: "Eismo įvykis",
+          reportLong: 24.279652,
+          reportLat: 54.687157,
+          reportDate: "2021-07-20",
+          isVisible: true,
+          isDeleted: false,
+          email: "pavyzdys@el.pastas.lt",
+          address: "Vilnius, Lietuva",
+          refId: "REF123",
+          status: "gautas",
+          phone: "+37061234567",
+          comment:
+              "Šiandien, apsilankius miške, pastebėjau išmestus šiferio lakštus. Jų gana nemažas kiekis, maždaug apie 100 lakštų. Jie išmėtyti po gana didelę teritoriją. Kartu su šiferio lakštais yra išmesti ir seni baldai bei buitinė technika.",
+          workingHours: "I-V 8:00-17:00",
+          moreInformation: "Papildoma informacija",
+          imageUrls: [
+            "https://nuoroda.lt/nuotrauka1.jpg",
+            "https://nuoroda.lt/nuotrauka2.jpg",
+          ],
+          officerImageUrls: [
+            "https://nuoroda.lt/policijos_nuotrauka1.jpg",
+          ],
+          historyData: [
+            HistoryData(
+              user: "Vartotojas1",
+              date: "2021-07-21",
+              edits: [
+                Edits(field: "status", change: "Naujas statusas"),
+                Edits(field: "komentaras", change: "Atnaujintas komentaras"),
+              ],
+            ),
+          ],
+          statusRecords: [
+            StatusRecords(status: "Naujas", date: "2021-07-20"),
+            StatusRecords(status: "Aptarnaujama", date: "2021-07-21"),
+          ],
+        ),
+        ReportModel(
+          id: "2",
+          name: "Pirmas ataskaitos modelis",
+          type: "Eismo įvykis",
           reportLong: 25.279652,
+          reportLat: 54.687157,
+          reportDate: "2021-07-20",
+          isVisible: true,
+          isDeleted: false,
+          email: "pavyzdys@el.pastas.lt",
+          address: "Vilnius, Lietuva",
+          refId: "REF123",
+          status: "gautas",
+          phone: "+37061234567",
+          comment:
+              "Šiandien, apsilankius miške, pastebėjau išmestus šiferio lakštus. Jų gana nemažas kiekis, maždaug apie 100 lakštų. Jie išmėtyti po gana didelę teritoriją. Kartu su šiferio lakštais yra išmesti ir seni baldai bei buitinė technika.",
+          workingHours: "I-V 8:00-17:00",
+          moreInformation: "Papildoma informacija",
+          imageUrls: [
+            "https://nuoroda.lt/nuotrauka1.jpg",
+            "https://nuoroda.lt/nuotrauka2.jpg",
+          ],
+          officerImageUrls: [
+            "https://nuoroda.lt/policijos_nuotrauka1.jpg",
+          ],
+          historyData: [
+            HistoryData(
+              user: "Vartotojas1",
+              date: "2021-07-21",
+              edits: [
+                Edits(field: "status", change: "Naujas statusas"),
+                Edits(field: "komentaras", change: "Atnaujintas komentaras"),
+              ],
+            ),
+          ],
+          statusRecords: [
+            StatusRecords(status: "Naujas", date: "2021-07-20"),
+            StatusRecords(status: "Aptarnaujama", date: "2021-07-21"),
+          ],
+        ),
+        ReportModel(
+          id: "3",
+          name: "Pirmas ataskaitos modelis",
+          type: "Eismo įvykis",
+          reportLong: 26.279652,
           reportLat: 54.687157,
           reportDate: "2021-07-20",
           isVisible: true,
@@ -72,93 +197,174 @@ class _UpdatedAdminScreenState extends State<UpdatedAdminScreen> {
   bool isShowDumps = true;
   bool isMapView = false;
   bool isShowDeleted = false;
+  Set<Marker> reportMarkers = {};
+  Set<Marker> dumpMarkers = {};
+
+  Future<void> setupMarker() async {
+    final description = await BitmapDescriptor.fromAssetImage(
+        const ImageConfiguration(size: Size(50, 50)),
+        'assets/svg/pin_icon.svg');
+
+    for (final report in mockReportModels) {
+      reportMarkers.add(Marker(
+        markerId: MarkerId(report.id),
+        position: LatLng(report.reportLat, report.reportLong),
+        icon: description,
+        onTap: () {
+          context.goNamed('report_admin',
+              extra: report, pathParameters: {'id': report.id});
+        },
+      ));
+    }
+    for (final dump in mockDumpModels) {
+      dumpMarkers.add(Marker(
+        markerId: MarkerId(dump.id),
+        position: LatLng(dump.reportLat, dump.reportLong),
+        icon: description,
+        onTap: () {
+          context.goNamed('dump', extra: dump, pathParameters: {'id': dump.id});
+        },
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupMarker();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BaseAdminScreen(
-        child: Padding(
-      padding: const EdgeInsets.symmetric(),
-      child: Center(
-        child: Container(
-          constraints: const BoxConstraints(
-            maxWidth: 1300,
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Header(
-                email: email,
-                name: name,
-                onLogout: () {
-                  //TODO: Add logout logic
-                },
+    return Scaffold(
+      body: LayoutBuilder(builder: (context, constraints) {
+        const minHeight = 300;
+        final height = constraints.maxHeight * .6 > minHeight
+            ? constraints.maxHeight * .6
+            : minHeight;
+        return BaseAdminScreen(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(),
+          child: Center(
+            child: Container(
+              constraints: const BoxConstraints(
+                maxWidth: 1300,
               ),
-              48.heightBox,
-              Text("Administracinė konsolė", style: CustomStyles.h2),
-              40.heightBox,
-              Row(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  UpdatedReportTypeSwitch(
-                    isShowDumps: isShowDumps,
-                    onReportTypeChange: (value) {
-                      setState(() {
-                        isShowDumps = value;
-                      });
+                  Header(
+                    email: email,
+                    name: name,
+                    onLogout: () {
+                      //TODO: Add logout logic
                     },
                   ),
-                  20.widthBox,
-                  CustomSwitch(
-                    value: isShowDeleted,
-                    width: 64,
-                    height: 32,
-                    activeThumbColor: CustomColors.orange,
-                    activeTrackColor: CustomColors.white,
-                    inactiveTrackColor: CustomColors.white.withOpacity(.1),
-                    inactiveThumbColor: CustomColors.primary.withOpacity(.4),
-                    onChanged: (value) {
-                      setState(() {
-                        isShowDeleted = value;
-                      });
-                    },
+                  48.heightBox,
+                  Text("Administracinė konsolė", style: CustomStyles.h2),
+                  40.heightBox,
+                  Row(
+                    children: [
+                      UpdatedReportTypeSwitch(
+                        isShowDumps: isShowDumps,
+                        onReportTypeChange: (value) {
+                          setState(() {
+                            isShowDumps = value;
+                          });
+                        },
+                      ),
+                      20.widthBox,
+                      CustomSwitch(
+                        value: isShowDeleted,
+                        width: 64,
+                        height: 32,
+                        activeThumbColor: CustomColors.orange,
+                        activeTrackColor: CustomColors.white,
+                        inactiveTrackColor: CustomColors.white.withOpacity(.1),
+                        inactiveThumbColor:
+                            CustomColors.primary.withOpacity(.4),
+                        onChanged: (value) {
+                          setState(() {
+                            isShowDeleted = value;
+                          });
+                        },
+                      ),
+                      12.widthBox,
+                      Text(
+                        "Rodyti ištrintus pranešimus",
+                        style: CustomStyles.body2
+                            .copyWith(color: CustomColors.white),
+                      ),
+                      const Spacer(),
+                      UpdatedAdminViewTypeSwitch(
+                        isMapView: isMapView,
+                        onIsMapViewChange: (value) {
+                          setState(() {
+                            isMapView = value;
+                          });
+                        },
+                      ),
+                    ],
                   ),
-                  12.widthBox,
-                  Text(
-                    "Rodyti ištrintus pranešimus",
-                    style:
-                        CustomStyles.body2.copyWith(color: CustomColors.white),
-                  ),
-                  const Spacer(),
-                  UpdatedAdminViewTypeSwitch(
-                    isMapView: isMapView,
-                    onIsMapViewChange: (value) {
-                      setState(() {
-                        isMapView = value;
-                      });
-                    },
-                  ),
+                  if (isMapView)
+                    _BuildMap(
+                      height: height.toDouble(),
+                      markers: isShowDumps ? dumpMarkers : reportMarkers,
+                      initialTarget: isShowDumps
+                          ? dumpMarkers.first.position
+                          : reportMarkers.first.position,
+                    )
+                  else
+                    AdminTable(
+                      reports: isShowDumps ? mockDumpModels : mockReportModels,
+                      // onReportClick: (reportModel) {
+                      //   context.goNamed('dump',
+                      //       extra: reportModel,
+                      //       pathParameters: {'id': reportModel.id});
+                      // },
+                    ),
+                  // CustomButton(
+                  //   text: 'Eiti i report',
+                  //   onPressed: () {
+                  //     context.goNamed('dump',
+                  //         extra: mockReportModels.first,
+                  //         pathParameters: {'id': mockReportModels.first.id});
+                  //   },
+                  // ),
                 ],
               ),
-              AdminTable(
-                reports: mockReportModels,
-                // onReportClick: (reportModel) {
-                //   context.goNamed('dump',
-                //       extra: reportModel,
-                //       pathParameters: {'id': reportModel.id});
-                // },
-              ),
-              // CustomButton(
-              //   text: 'Eiti i report',
-              //   onPressed: () {
-              //     context.goNamed('dump',
-              //         extra: mockReportModels.first,
-              //         pathParameters: {'id': mockReportModels.first.id});
-              //   },
-              // ),
-            ],
+            ),
           ),
+        ));
+      }),
+    );
+  }
+}
+
+class _BuildMap extends StatelessWidget {
+  const _BuildMap({
+    required this.height,
+    required this.markers,
+    required this.initialTarget,
+  });
+
+  final double height;
+  final Set<Marker> markers;
+  final LatLng initialTarget;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height.toDouble(),
+      child: GoogleMap(
+        mapType: MapType.none,
+        markers: markers,
+        initialCameraPosition: CameraPosition(
+          target: initialTarget,
+          zoom: 13,
         ),
       ),
-    ));
+    );
   }
 }
