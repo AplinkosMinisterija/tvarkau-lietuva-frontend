27625daf8c0dd411ca4c4742e11401c41022633a
Karolis Vyčius <k.vycius@gmail.com>
2024-01-18 18:07:15 +0200

    flutter_map_cancellable_tile_provider

--- core_ui/lib/src/widgets/map/app_map.dart
+++ core_ui/lib/src/widgets/map/app_map.dart
@@ -1,6 +1,7 @@
 import 'package:core/constants/global_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
@@ -141,6 +142,9 @@ class _AppMapState extends State<AppMap> {
           children: [
             TileLayer(
               urlTemplate: _getTileUrlTemplate(),
+              tileProvider: CancellableNetworkTileProvider(
+                silenceExceptions: true,
+              ),
               tileSize: 512,
             ),
             ...widget.layers,
