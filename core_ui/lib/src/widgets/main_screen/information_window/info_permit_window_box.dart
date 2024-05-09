5ec2464f1f012f3ef5e30b2fc56c1258e085d26b
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2024-05-09 18:23:04 +0300

    Add mock permit layer with custom preview window

---
+++ core_ui/lib/src/widgets/main_screen/information_window/info_permit_window_box.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InfoPermitWindowBox extends StatelessWidget {
+  InfoPermitWindowBox({
+    super.key,
+    required this.type,
+    required this.issuedFrom,
+    required this.issuedTo,
+    required this.cadastralNumber,
+    required this.subdivision,
+    required this.forestryDistrict,
+    required this.block,
+    required this.plot,
+    required this.cuttableArea,
+    required this.dominantTree,
+    required this.cuttingType,
+    required this.reinstatementType,
+  });
+
+  final String? type;
+  final String? issuedFrom;
+  final String? issuedTo;
+  final String? cadastralNumber;
+  final String? subdivision;
+  final String? forestryDistrict;
+  final String? block;
+  final String? plot;
+  final String? cuttableArea;
+  final String? dominantTree;
+  final String? cuttingType;
+  final String? reinstatementType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 328,
+      height: 211,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14),
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.all(16),
+      child: ListView(
+        children: [
+          getListTile(titles[0], type ?? '-'),
+          getListTile(titles[1], issuedFrom ?? '-'),
+          getListTile(titles[2], issuedTo ?? '-'),
+          getListTile(titles[3], cadastralNumber ?? '-'),
+          getListTile(titles[4], subdivision ?? '-'),
+          getListTile(titles[5], forestryDistrict ?? '-'),
+          getListTile(titles[6], block ?? '-'),
+          getListTile(titles[7], plot ?? '-'),
+          getListTile(titles[8], cuttableArea ?? '-'),
+          getListTile(titles[9], dominantTree ?? '-'),
+          getListTile(titles[10], cuttingType ?? '-'),
+          getListTile(titles[11], reinstatementType ?? '-'),
+        ],
+      ),
+    );
+  }
+
+  List<String> titles = [
+    'Tipas',
+    'Galioja nuo',
+    'Galioja iki',
+    'Kadastrinis Nr.',
+    'VMU padalinys',
+    'Girininkija',
+    'Kvartalas',
+    'Sklypas',
+    'Kertamas plotas',
+    'Vyraujantys medžiai',
+    'Kirtimo rūšis',
+    'Atkūrimo būdas'
+  ];
+
+  Widget getListTile(String title, String description) {
+    return Row(
+      children: [
+        getListTileItem(title),
+        Divider(
+          height: 50,
+          thickness: 2,
+          color: Colors.black,
+        ),
+        getListTileItem(description),
+      ],
+    );
+  }
+
+  Widget getListTileItem(String content) {
+    return Container(
+      margin: const EdgeInsets.all(2),
+      width: 140,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+        color: Colors.grey.withOpacity(0.1),
+      ),
+      child: Text(
+        content,
+        textAlign: TextAlign.center,
+        style: getTextStyle(),
+      ),
+    );
+  }
+
+  TextStyle getTextStyle() {
+    return GoogleFonts.roboto(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+    );
+  }
+}
--- features/dashboard/lib/src/adding_report/ui/mobile/add_pin_screen_mobile.dart
+++ features/dashboard/lib/src/adding_report/ui/mobile/add_pin_screen_mobile.dart
@@ -127,7 +127,7 @@ class _AddPinScreenMobileState extends State<AddPinScreenMobile> {
                       ),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
