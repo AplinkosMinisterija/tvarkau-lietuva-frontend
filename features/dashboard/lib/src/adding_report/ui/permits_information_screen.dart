e104f753e329451c9cb87341a6ae6eb6b7411665
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2024-07-11 18:26:38 +0300

    image size optimization

--- features/dashboard/lib/src/adding_report/ui/permits_information_screen.dart
+++ features/dashboard/lib/src/adding_report/ui/permits_information_screen.dart
@@ -68,10 +68,10 @@ class PermitsInformationScreen extends StatelessWidget {
               const SizedBox(height: 12),
               ImageGallery().buildImages(
                 imageUrls: [
-                  'https://res.cloudinary.com/dms8ku4iv/image/upload/v1714134279/medziai.jpg',
-                  'https://res.cloudinary.com/dms8ku4iv/image/upload/v1714134279/nuvirtusi-egle.jpg',
-                  'https://res.cloudinary.com/dms8ku4iv/image/upload/v1711572859/lblbzwcw416t2tmiyras.jpg',
-                  'https://res.cloudinary.com/dms8ku4iv/image/upload/v1711572860/bm0u9sclakngjvtwpiup.jpg',
+                  'https://res.cloudinary.com/dms8ku4iv/image/upload/q_40:420/v1714134279/medziai.jpg',
+                  'https://res.cloudinary.com/dms8ku4iv/image/upload/q_40:420/v1714134279/nuvirtusi-egle.jpg',
+                  'https://res.cloudinary.com/dms8ku4iv/image/upload/q_auto:best/v1711572859/lblbzwcw416t2tmiyras.jpg',
+                  'https://res.cloudinary.com/dms8ku4iv/image/upload/q_auto:best/v1711572860/bm0u9sclakngjvtwpiup.jpg',
                 ],
                 context: context,
                 width: 300,
