5f883f8ae50b5b0339dd1f75ed492115d83e6ad8
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2024-07-17 15:58:24 +0300

    footer layout changes

--- core_ui/lib/src/widgets/main_screen/footer/footer_consultation.dart
+++ core_ui/lib/src/widgets/main_screen/footer/footer_consultation.dart
@@ -16,13 +16,14 @@ class FooterConsultationInformation extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: isMobile ? width * 0.911 : width * 0.5578,
-      height: isMobile ? width * 0.4027 : width * 0.104,
+      height: isMobile ? width * 0.4027 : width * 0.11,
       decoration: BoxDecoration(
         color: const Color.fromRGBO(57, 97, 84, 0.07),
         borderRadius: BorderRadius.circular(14),
       ),
       padding: EdgeInsets.symmetric(
         horizontal: isMobile ? width * 0.05 : width * 0.01875,
+        vertical: isMobile ? width * 0.01 : width * 0.01875,
       ),
       child: SelectionArea(
         child: Column(
@@ -86,34 +87,60 @@ class FooterConsultationInformation extends StatelessWidget {
                     ),
                   ),
                 ),
+                if (!isMobile) ...[
+                  SizedBox(width: width * 0.0304),
+                  Icon(
+                    Icons.campaign,
+                    size: isMobile ? width * 0.0556 : width * 0.0156,
+                    color: const Color.fromRGBO(255, 106, 61, 1),
+                  ),
+                  SizedBox(width: isMobile ? width * 0.03 : width * 0.0094),
+                  InkWell(
+                    onTap: () {
+                      LaunchUrl().launch('tel:+112');
+                    },
+                    child: Text(
+                      'Pastebėję galimą aplinkosauginį\npažeidimą, praneškite tel. 112',
+                      style: GoogleFonts.roboto(
+                        fontSize:
+                        isMobile ? width * 0.031 : width * 0.01093,
+                        fontWeight: FontWeight.w400,
+                        color: const Color.fromRGBO(0, 0, 0, 1),
+                      ),
+                    ),
+                  ),
+                ]
               ],
             ),
             SizedBox(height: isMobile ? width * 0.0333 : width * 0.0125),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.campaign,
-                  size: isMobile ? width * 0.0556 : width * 0.0156,
-                  color: const Color.fromRGBO(255, 106, 61, 1),
-                ),
-                SizedBox(width: isMobile ? width * 0.03 : width * 0.0094),
-                InkWell(
-                  onTap: () {
-                    LaunchUrl().launch('tel:+112');
-                  },
-                  child: Text(
-                    'Pastebėję galimą aplinkosauginį pažeidimą,\npraneškite tel. 112',
-                    style: GoogleFonts.roboto(
-                      fontSize: isMobile ? width * 0.031 : width * 0.01093,
-                      fontWeight: FontWeight.w400,
-                      color: const Color.fromRGBO(0, 0, 0, 1),
-                    ),
-                  ),
-                ),
-              ],
-            )
+            isMobile
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.campaign,
+                        size: isMobile ? width * 0.0556 : width * 0.0156,
+                        color: const Color.fromRGBO(255, 106, 61, 1),
+                      ),
+                      SizedBox(width: isMobile ? width * 0.03 : width * 0.0094),
+                      InkWell(
+                        onTap: () {
+                          LaunchUrl().launch('tel:+112');
+                        },
+                        child: Text(
+                          'Pastebėję galimą aplinkosauginį pažeidimą,\npraneškite tel. 112',
+                          style: GoogleFonts.roboto(
+                            fontSize:
+                                isMobile ? width * 0.031 : width * 0.01093,
+                            fontWeight: FontWeight.w400,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                : const SizedBox.shrink()
           ],
         ),
       ),
