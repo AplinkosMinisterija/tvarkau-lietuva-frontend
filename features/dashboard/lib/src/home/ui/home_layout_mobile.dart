6df3542bfbc48774ac69222fbed9cc467aaa38db
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2024-07-30 11:19:27 +0300

    flutter map integration testing

--- features/dashboard/lib/src/home/ui/home_layout_mobile.dart
+++ features/dashboard/lib/src/home/ui/home_layout_mobile.dart
@@ -1,8 +1,14 @@
+import 'dart:math';
+
 import 'package:core/core.dart';
 import 'package:core_ui/core_ui.dart';
 import 'package:dashboard/src/home/ui/map_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+import '../tile_provider.dart';
 
 class HomeLayoutMobile extends StatefulWidget {
   const HomeLayoutMobile({
@@ -19,8 +25,9 @@ class HomeLayoutMobile extends StatefulWidget {
 }
 
 class _HomeLayoutMobileState extends State<HomeLayoutMobile> {
-  late bool isMapHover;
-  late ScrollController scrollController;
+  // late bool isMapHover;
+  // late ScrollController scrollController;
+  List<Marker> allMarkers = [];
 
   // Future<void> updateScrollControllerOffset(double offset) async {
   //   await SecureStorageProvider().setScrollOffset(offset);
@@ -28,17 +35,31 @@ class _HomeLayoutMobileState extends State<HomeLayoutMobile> {
 
   @override
   void initState() {
-    scrollController =
-        ScrollController(initialScrollOffset: widget.scrollOffset);
-    isMapHover = false;
+    // scrollController =
+    //     ScrollController(initialScrollOffset: widget.scrollOffset);
+    // isMapHover = false;
     super.initState();
+    Future.microtask(() {
+      final r = Random();
+      for (var x = 0; x < 1000; x++) {
+        allMarkers.add(
+          Marker(
+            point: LatLng(r.nextDouble() * 54, r.nextDouble() * 24),
+            height: 12,
+            width: 12,
+            child: ColoredBox(color: Colors.blue[900]!),
+          ),
+        );
+      }
+      setState(() {});
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       //physics: isMapHover ? const NeverScrollableScrollPhysics() : null,
-      controller: scrollController,
+      //controller: scrollController,
       child: Stack(
         children: [
           BackgroundWidget(
@@ -76,6 +97,34 @@ class _HomeLayoutMobileState extends State<HomeLayoutMobile> {
                 //   width: widget.width,
                 //   isMobile: true,
                 // ),
+                SizedBox(
+                  height: widget.width * 1.722,
+                  width: widget.width * 0.9112,
+                  child: Stack(
+                    children: [
+                      FlutterMap(
+                        options: MapOptions(
+                          initialCameraFit: CameraFit.bounds(
+                            bounds: LatLngBounds(
+                              const LatLng(55, -9),
+                              const LatLng(37, 30),
+                            ),
+                            padding: const EdgeInsets.only(
+                              left: 16,
+                              right: 16,
+                              top: 88,
+                              bottom: 192,
+                            ),
+                          ),
+                        ),
+                        children: [
+                          openStreetMapTileLayer,
+                          MarkerLayer(markers: allMarkers),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
                 SizedBox(height: widget.width * 0.0343),
                 Footer(
                   width: widget.width,
