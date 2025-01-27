7837de78432e0c804b143300b1e25ca90f1a0132
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2025-01-27 18:11:41 +0200

    update image optimisation logic

--- core_ui/lib/src/widgets/app_network_image.dart
+++ core_ui/lib/src/widgets/app_network_image.dart
@@ -18,36 +18,40 @@ class AppNetworkImage extends StatelessWidget {
       url,
       cacheKey: url,
     );
-    return Image(
-        image: ResizeImage(
-      imageProvider,
-      width: 150,
-      height: 150,
-      policy: ResizeImagePolicy.fit,
-    ));
-    //   CachedNetworkImage(
-    //   imageUrl: url,
-    //   imageBuilder: (context, imageProvider) => Container(
-    //     decoration: BoxDecoration(
-    //       image: DecorationImage(
-    //         image: imageProvider,
-    //         fit: fit ?? BoxFit.contain,
-    //       ),
-    //     ),
-    //   ),
-    //   placeholder: (context, url) => Container(
-    //     decoration: BoxDecoration(
-    //         borderRadius: BorderRadius.circular(10),
-    //         border: Border.all(
-    //           color: const Color.fromRGBO(0, 0, 0, 1),
-    //           width: 1,
-    //         )),
-    //     child: LoadingAnimationWidget.fourRotatingDots(
-    //       color: const Color.fromRGBO(57, 97, 84, 1),
-    //       size: 50,
-    //     ),
-    //   ),
-    //   errorWidget: (context, url, error) => const Icon(Icons.error),
-    // );
+    // return Image(
+    //     image: ResizeImage(
+    //   imageProvider,
+    //   width: 150,
+    //   height: 150,
+    //   policy: ResizeImagePolicy.fit,
+    // ));
+    return CachedNetworkImage(
+      memCacheWidth: 45,
+      memCacheHeight: 60,
+      maxHeightDiskCache: 60,
+      maxWidthDiskCache: 45,
+      imageUrl: url,
+      imageBuilder: (context, imageProv) => Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: imageProvider,
+            fit: fit ?? BoxFit.contain,
+          ),
+        ),
+      ),
+      placeholder: (context, url) => Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: const Color.fromRGBO(0, 0, 0, 1),
+              width: 1,
+            )),
+        child: LoadingAnimationWidget.fourRotatingDots(
+          color: const Color.fromRGBO(57, 97, 84, 1),
+          size: 50,
+        ),
+      ),
+      errorWidget: (context, url, error) => const Icon(Icons.error),
+    );
   }
 }
