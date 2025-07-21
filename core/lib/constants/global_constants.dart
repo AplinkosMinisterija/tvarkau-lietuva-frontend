cad3a5de4e5b32ca5a68a36b6f62c8c52ad8c768
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2025-07-21 15:03:05 +0300

    Update global_constants.dart

--- core/lib/constants/global_constants.dart
+++ core/lib/constants/global_constants.dart
@@ -4,6 +4,7 @@ class GlobalConstants {
   static const int maxAllowedImageCount = 4;
   static const int maxAllowedImageSizeInBytes = 20000000;
   static const String basePath = 'https://dev-api.tvarkaulietuva.lt';
+  
   static const String adminBeetleAccount = 'zievegrauzis@aad.am.lt';
   static const String woodcuttingPermitsUrl =
       'https://lkmp.tvarkaulietuva.lt/lkmp-data.geojson';
