bf341c93eb4bbbc1132815b3a3fd1220a705e5d0
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2026-04-08 05:34:43 +0300

    Update footer description, texts, spacing

--- core_ui/lib/src/widgets/main_screen/footer/footer_description.dart
+++ core_ui/lib/src/widgets/main_screen/footer/footer_description.dart
@@ -33,7 +33,7 @@ class FooterDescription extends StatelessWidget {
   List<Widget> getDescriptionWidgets(double width, bool isMobile) {
     return [
       SizedBox(
-        width: isMobile ? width * 0.8889 : width * 0.3133,
+        width: isMobile ? width * 0.8889 : width * 0.5133,
         height: isMobile ? width * 0.3333 : width * 0.0797,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -56,62 +56,6 @@ class FooterDescription extends StatelessWidget {
                 fontSize: isMobile ? width * 0.0333 : width * 0.009375,
                 fontWeight: FontWeight.w400,
                 color: const Color.fromRGBO(0, 0, 0, 1),
-              ),
-            ),
-          ],
-        ),
-      ),
-      SizedBox(
-        width: isMobile ? width * 0.8333 : width * 0.2335,
-        height: isMobile ? width * 0.1722 : width * 0.04844,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'smalsuolis.lt',
-              textAlign: TextAlign.left,
-              style: GoogleFonts.roboto(
-                fontSize: isMobile ? width * 0.03889 : width * 0.01093,
-                fontWeight: FontWeight.w700,
-                color: const Color.fromRGBO(28, 63, 58, 1),
-              ),
-            ),
-            SizedBox(height: isMobile ? width * 0.01944 : width * 0.00468),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text:
-                        'Domina kas vyksta tave dominančioje teritorijoje realiu laiku? tapk ',
-                    style: GoogleFonts.roboto(
-                      fontSize: isMobile ? width * 0.0333 : width * 0.009375,
-                      fontWeight: FontWeight.w400,
-                      color: const Color.fromRGBO(0, 0, 0, 1),
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'smalsuolis.lt',
-                    style: GoogleFonts.roboto(
-                      fontSize: isMobile ? width * 0.0333 : width * 0.009375,
-                      fontWeight: FontWeight.w700,
-                      color: const Color.fromRGBO(28, 63, 58, 1),
-                      decoration: TextDecoration.underline,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        LaunchUrl().launch('https://smalsuolis.lt/');
-                      },
-                  ),
-                  TextSpan(
-                    text: ' nariu.',
-                    style: GoogleFonts.roboto(
-                      fontSize: isMobile ? width * 0.0333 : width * 0.009375,
-                      fontWeight: FontWeight.w400,
-                      color: const Color.fromRGBO(0, 0, 0, 1),
-                    ),
-                  ),
-                ],
               ),
             ),
           ],
